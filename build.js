0dad33ea13e0c5247761e7fb42370ff266849ff0
James Pearce <github@tripleodeon.com>
2022-05-16 15:20:34 -0700

    Extra build flags

--- build.js
+++ build.js
@@ -3,4 +3,4 @@ const { exec, execSync } = require("child_process");
 
 var rootDir = __dirname;
 
-execSync(`${rootDir}/node_modules/.bin/browserify ${rootDir}/testFile.js > output.js`);
+execSync(`${rootDir}/node_modules/.bin/browserify ${rootDir}/testFile.js -g [ babelify --presets [ @babel/preset-env ] ] > output.js`);
